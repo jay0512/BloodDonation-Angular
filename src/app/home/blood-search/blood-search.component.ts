b4a91f11acae96ed4e629513fb4a01dbccea4617
jay0512 <jaypatelvicky5@gmail.com>
2018-09-28 09:21:27 +0530

    project increment

---
+++ src/app/home/blood-search/blood-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blood-search',
+  templateUrl: './blood-search.component.html',
+  styleUrls: ['./blood-search.component.css']
+})
+export class BloodSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
